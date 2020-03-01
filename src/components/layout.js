19a74eec71d9db5b61fecd41c5615bee67cddd0d
ThomasAndrewMacLean <31767869+ThomasAndrewMacLean@users.noreply.github.com>
2020-03-01 15:24:41 +0100

    add mobile nav

--- src/components/layout.js
+++ src/components/layout.js
@@ -59,7 +59,7 @@ const TemplateWrapper = ({ children, location }) => {
                                 {data.datoCmsSite.globalSeo.siteName}
                             </Link>
                         </h1>
-                        <nav>
+                        <nav className="main-nav">
                             <ul>
                                 <li
                                     className={
@@ -101,66 +101,60 @@ const TemplateWrapper = ({ children, location }) => {
                                 </li>
                             </ul>
                         </nav>
+                        <div
+                            onClick={() => setShowMenu(!showMenu)}
+                            className={
+                                showMenu
+                                    ? 'menu-open clickHamburger'
+                                    : 'clickHamburger'
+                            }
+                        >
+                            <div className="hamburger"></div>
+                        </div>
                     </header>
-                    {/* <div className="container__sidebar"> 
-            <div className="sidebar">
-              <h6 className="sidebar__title">
-                <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-              </h6>
-              <div
-                className="sidebar__intro"
-                dangerouslySetInnerHTML={{
-                  __html:
-                    data.datoCmsHome.introTextNode.childMarkdownRemark.html
-                }}
-              />
-              <ul className="sidebar__menu">
-                <li>
-                  <Link to="/">Home:</Link>
-                </li>
-                <li>
-                  <Link to="/about">About</Link>
-                </li>
-              </ul>
-              <p className="sidebar__social">
-                {data.allDatoCmsSocialProfile.edges.map(({ node: profile }) => (
-                  <a
-                    key={profile.profileType}
-                    href={profile.url}
-                    target="blank"
-                    className={`social social--${profile.profileType.toLowerCase()}`}
-                  >
-                    {" "}
-                  </a>
-                ))}
-              </p>
-              <div className="sidebar__copyright">
-                {data.datoCmsHome.copyright.replace(
-                  "YEAR",
-                  new Date().getFullYear()
-                )}
-              </div>
-            </div>
-          </div>*/}
-                    <div className="container__body">
-                        {/* <div className="container__mobile-header">
-              <div className="mobile-header">
-                <div className="mobile-header__menu">
-                  <a
-                    href="#"
-                    onClick={e => {
-                      e.preventDefault();
-                      setShowMenu(!showMenu);
-                    }}
-                  />
-                </div>
-                <div className="mobile-header__logo">
-                  <Link to="/">{data.datoCmsSite.globalSeo.siteName}</Link>
-                </div>
-              </div>
-            </div> */}
-                        {children}
-                    </div>
+                    <nav
+                        className={
+                            showMenu ? 'menu-open mobile-nav' : 'mobile-nav'
+                        }
+                    >
+                        <ul>
+                            <li
+                                className={
+                                    location.pathname === '/' ? 'active' : ''
+                                }
+                            >
+                                <Link to="/">home</Link>
+                            </li>
+                            <li
+                                className={
+                                    location.pathname.includes('/tekeningen')
+                                        ? 'active'
+                                        : ''
+                                }
+                            >
+                                <Link to="/tekeningen">tekeningen</Link>
+                            </li>
+                            <li
+                                className={
+                                    location.pathname.includes('/workshops')
+                                        ? 'active'
+                                        : ''
+                                }
+                            >
+                                <Link to="/workshops">workshops</Link>
+                            </li>
+                            <li
+                                className={
+                                    location.pathname.includes('/contact')
+                                        ? 'active'
+                                        : ''
+                                }
+                            >
+                                <Link to="/contact">contact</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <div className="container__body">{children}</div>
                     <footer>
                         <span>{data.datoCmsHome.addressText}</span>
                         <span>{data.datoCmsHome.telephone}</span>
