a8fb6bbc370b34e81c0a15a97136e2dd91f51ecc
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-05-22 18:50:17 +0200

    order

--- src/pages/tekeningen.js
+++ src/pages/tekeningen.js
@@ -1,57 +1,63 @@
-import React from 'react';
-import { graphql } from 'gatsby';
-import Masonry from 'react-masonry-component';
-import Img from 'gatsby-image';
-import Layout from '../components/layout';
+import React from "react";
+import { graphql } from "gatsby";
+import Masonry from "react-masonry-component";
+import Img from "gatsby-image";
+import Layout from "../components/layout";
 
 const TekeningenPage = ({ data, location }) => {
-    console.log(data);
-    function shuffle(a) {
-        for (let i = a.length - 1; i > 0; i--) {
-            const j = Math.floor(Math.random() * (i + 1));
-            [a[i], a[j]] = [a[j], a[i]];
-        }
-        return a;
+  console.log(data);
+  function shuffle(a) {
+    for (let i = a.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [a[i], a[j]] = [a[j], a[i]];
     }
-    return (
-        <Layout location={location}>
-            <Masonry className="showcase">
-                <div className="showcase__item">
-                    <h1>{data.datoCmsTekeningen.titel}</h1>
-                </div>
-                {shuffle(data.datoCmsTekeningen.tekeningen).map(tekening => (
-                    <div key={tekening.filename} className="showcase__item">
-                        <figure className="card">
-                            <Img fluid={tekening.fluid} />
-                        </figure>
-                    </div>
-                ))}
-            </Masonry>
-        </Layout>
-    );
+    return a;
+  }
+  return (
+    <Layout location={location}>
+      <Masonry className="showcase">
+        <div className="showcase__item">
+          <h1
+            className="tekening-title"
+            dangerouslySetInnerHTML={{
+              __html: data.datoCmsTekeningen.titelNode.childMarkdownRemark.html,
+            }}
+          ></h1>
+        </div>
+        {shuffle(data.datoCmsTekeningen.tekeningen).map((tekening) => (
+          <div key={tekening.filename} className="showcase__item">
+            <figure className="card">
+              <Img fluid={tekening.fluid} />
+            </figure>
+          </div>
+        ))}
+      </Masonry>
+    </Layout>
+  );
 };
 
 export default TekeningenPage;
 
 export const query = graphql`
-    query TekeningenQuery {
-        datoCmsTekeningen {
-            titel
-            uitlegNode {
-                childMarkdownRemark {
-                    html
-                }
-            }
-            tekeningen {
-                filename
-                url
-                fluid(
-                    maxWidth: 300
-                    imgixParams: { fm: "jpg", auto: "compress" }
-                ) {
-                    ...GatsbyDatoCmsSizes
-                }
-            }
+  query TekeningenQuery {
+    datoCmsTekeningen {
+      titelNode {
+        childMarkdownRemark {
+          html
         }
+      }
+      uitlegNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+      tekeningen {
+        filename
+        url
+        fluid(maxWidth: 300, imgixParams: { fm: "jpg", auto: "compress" }) {
+          ...GatsbyDatoCmsSizes
+        }
+      }
     }
+  }
 `;
--- src/pages/workshops.js
+++ src/pages/workshops.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { Link, graphql } from 'gatsby';
-import Masonry from 'react-masonry-component';
-import Img from 'gatsby-image';
-import Layout from '../components/layout';
+import React from "react";
+import { Link, graphql } from "gatsby";
+import Masonry from "react-masonry-component";
+import Img from "gatsby-image";
+import Layout from "../components/layout";
 
 const WorkshopsPage = ({ data, location }) => {
-    console.log(data);
-    return (
-        <Layout location={location}>
-            <Masonry className="showcase">
-                {/* <div className="showcase__item">
+  console.log(data);
+  return (
+    <Layout location={location}>
+      <Masonry className="showcase">
+        {/* <div className="showcase__item">
                     <div
                         className="intro"
                         dangerouslySetInnerHTML={{
@@ -19,59 +19,71 @@ const WorkshopsPage = ({ data, location }) => {
                         }}
                     ></div>
                 </div> */}
-                <div className="showcase__item">
-                    <h1>We hebben een passie voor workshops</h1>
-                </div>
-                {data.allDatoCmsWorkshop.edges.map(({ node: work }) => (
-                    <div key={work.id} className="showcase__item">
-                        <figure className="card">
-                            <Link
-                                to={`/workshops/${work.titel
-                                    .split('?')
-                                    .join('')
-                                    .split(' ')
-                                    .join('_')}`}
-                                className="card__image"
-                            >
-                                <Img fluid={work.afbeelding.fluid} />
-                                <figcaption className="card__caption">
-                                    <h6 className="card__title">
-                                        {work.titel}
-                                    </h6>
-                                    <div className="card__description">
-                                        <p>{work.ondertitel}</p>
-                                    </div>
-                                </figcaption>
-                            </Link>
-                        </figure>
-                    </div>
-                ))}
-            </Masonry>
-        </Layout>
-    );
+        <div className="showcase__item">
+          <h1 className="workshops-title">
+            We hebben een passie voor workshops
+          </h1>
+        </div>
+        {data.allDatoCmsWorkshop.edges.map(({ node: work }) => (
+          <div key={work.id} className="showcase__item">
+            <figure
+              className={
+                new Date(work.datum) < new Date()
+                  ? "workshop-voorbij card"
+                  : "card"
+              }
+            >
+              <Link
+                to={`/workshops/${work.titel
+                  .split("?")
+                  .join("")
+                  .split(" ")
+                  .join("_")}`}
+                className="card__image"
+              >
+                <Img fluid={work.afbeelding.fluid} />
+                <figcaption className="card__caption">
+                  <h6 className="card__title">{work.titel}</h6>
+                  {work.datum && (
+                    <date>
+                      {new Date(work.datum).toLocaleString("nl-BE", {
+                        month: "long",
+                        year: "numeric",
+                      })}
+                    </date>
+                  )}
+                  <div className="card__description">
+                    <p>{work.ondertitel}</p>
+                  </div>
+                </figcaption>
+              </Link>
+            </figure>
+          </div>
+        ))}
+      </Masonry>
+    </Layout>
+  );
 };
 
 export default WorkshopsPage;
 
 export const query = graphql`
-    query WorkshopsQuery {
-        allDatoCmsWorkshop {
-            edges {
-                node {
-                    id
-                    ondertitel
-                    titel
-                    afbeelding {
-                        url
-                        fluid(
-                            maxWidth: 300
-                            imgixParams: { fm: "jpg", auto: "compress" }
-                        ) {
-                            ...GatsbyDatoCmsSizes
-                        }
-                    }
-                }
+  query WorkshopsQuery {
+    allDatoCmsWorkshop(sort: { fields: datum, order: ASC }) {
+      edges {
+        node {
+          id
+          ondertitel
+          titel
+          datum
+          afbeelding {
+            url
+            fluid(maxWidth: 300, imgixParams: { fm: "jpg", auto: "compress" }) {
+              ...GatsbyDatoCmsSizes
             }
+          }
         }
+      }
     }
+  }
 `;
