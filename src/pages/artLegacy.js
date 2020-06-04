d026bf74cdd100b046e6a5a35b7dbf35d82f4e64
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-06-04 17:46:29 +0200

    add to cMs

--- src/pages/artLegacy.js
+++ src/pages/artLegacy.js
@@ -25,6 +25,91 @@ const LegacyPage = ({ data, location }) => {
               )}
             </div>
           </div>
+
+          {/* AMBASSADEUR */}
+          <div className="showcase__item">
+            <div
+              style={{
+                background: data.datoCmsArtIsYourLegacy.kleurAmbassadeur,
+              }}
+              className={"legacy-text" + " intro"}
+            >
+              {data.datoCmsArtIsYourLegacy.textAmbassadeurNode
+                .childMarkdownRemark.html && (
+                <div
+                  className="card__title"
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      data.datoCmsArtIsYourLegacy.textAmbassadeurNode
+                        .childMarkdownRemark.html,
+                  }}
+                />
+              )}
+            </div>
+          </div>
+          {/* PETER */}
+          <div className="showcase__item">
+            <div
+              style={{
+                background: data.datoCmsArtIsYourLegacy.kleurPeter,
+              }}
+              className={"legacy-text" + " intro"}
+            >
+              {data.datoCmsArtIsYourLegacy.textPeterNode.childMarkdownRemark
+                .html && (
+                <div
+                  className="card__title"
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      data.datoCmsArtIsYourLegacy.textPeterNode
+                        .childMarkdownRemark.html,
+                  }}
+                />
+              )}
+            </div>
+          </div>
+          {/* METER */}
+          <div className="showcase__item">
+            <div
+              style={{
+                background: data.datoCmsArtIsYourLegacy.kleurMeter,
+              }}
+              className={"legacy-text" + " intro"}
+            >
+              {data.datoCmsArtIsYourLegacy.textMeterNode.childMarkdownRemark
+                .html && (
+                <div
+                  className="card__title"
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      data.datoCmsArtIsYourLegacy.textMeterNode
+                        .childMarkdownRemark.html,
+                  }}
+                />
+              )}
+            </div>
+          </div>
+          {/* KUNSTENAAR */}
+          <div className="showcase__item">
+            <div
+              style={{
+                background: data.datoCmsArtIsYourLegacy.kleurKunstenaar,
+              }}
+              className={"legacy-text" + " intro"}
+            >
+              {data.datoCmsArtIsYourLegacy.textKunstenaarNode
+                .childMarkdownRemark.html && (
+                <div
+                  className="card__title"
+                  dangerouslySetInnerHTML={{
+                    __html:
+                      data.datoCmsArtIsYourLegacy.textKunstenaarNode
+                        .childMarkdownRemark.html,
+                  }}
+                />
+              )}
+            </div>
+          </div>
           {data.datoCmsArtIsYourLegacy.images.map((tekening, index) => {
             return (
               <div key={index} className="showcase__item">
@@ -50,6 +135,30 @@ export default LegacyPage;
 export const query = graphql`
   query LegacyQuery {
     datoCmsArtIsYourLegacy {
+      kleurAmbassadeur
+      kleurKunstenaar
+      kleurMeter
+      kleurPeter
+      textAmbassadeurNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+      textKunstenaarNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+      textMeterNode {
+        childMarkdownRemark {
+          html
+        }
+      }
+      textPeterNode {
+        childMarkdownRemark {
+          html
+        }
+      }
       textaNode {
         childMarkdownRemark {
           html
