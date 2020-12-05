fc4c6bab46271f56cde10a707300ecff45de7949
ThomasAndrewMacLean <hello@thomasmaclean.be>
2020-12-05 13:23:11 +0100

    video

--- src/pages/video.js
+++ src/pages/video.js
@@ -6,7 +6,10 @@ const Video = ({ location }) => {
     <Layout location={location}>
       <div className="pusher">
         <video id="video" controls src="agzilles.mp4" width="100%">
-          <source src="agzilles.mp4" type="video/mp4" />
+          <source
+            src="https://f000.backblazeb2.com/file/thomasmacleanbucket/agzilles.mp4"
+            type="video/mp4"
+          />
         </video>
       </div>
     </Layout>
